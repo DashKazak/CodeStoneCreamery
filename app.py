fcf7f76f9f562f308d26bbdaaee16fc1b41c11fa
wyatt.geiger <wyatt.geiger@go.minneapolis.edu>
2022-04-07 14:47:29 -0500

    Added some form data on to index.html. Created base.html which currently incorporates bootstrap. Created empty file for balance_envies.html which will include more info later

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/balance')
+def get_balance():
+    return render_template('balance_envies.html')
